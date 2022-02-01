dfef1e48084267fe2d1e01a8c250ff03dc543baf
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2022-02-01 16:51:25 +0530

    Create sensor_interface.ino

---
+++ Arduino/sensor_interface.ino
@@ -0,0 +1,24 @@
+// defines pins numbers
+const int trigPin = 9;
+const int echoPin = 10;
+// defines variables
+long duration;
+int distance;
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+}
+void loop() {
+  Serial.println("Testing");
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance= duration*0.034/2;
+  Serial.print("Distance from obstacle is ");
+  Serial.println(distance);
+  delay(2000);
+}
